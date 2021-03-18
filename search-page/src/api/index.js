3bb43c34fc83589a11d7ffe88b31a82ec15979ec
신유나 <yunadev01@gmail.com>
2021-03-19 00:16:57 +0900

    API 정리

--- search-page/src/api/index.js
+++ search-page/src/api/index.js
@@ -1,9 +1,29 @@
-const API_ENDPOINT = 'https://oivhcpn8r9.execute-api.ap-northeast-2.amazonaws.com/dev';
+const API_ENDPOINT = 'https://oivhcpn8r9.execute-api.ap-northeast-2.amazonaws.com/dev/api';
 
 const API = {};
 
-API.getSearchCats = keyword => {
-  return fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`).then(res => res.json());
+/**
+ * @description Keyword로 검색된 고양이 사진 목록
+ * @param {String} keyword
+ * @returns {Array}
+ */
+API.getCats = keyword => {
+  return fetch(`${API_ENDPOINT}/cats/search?q=${keyword}`).then(res => res.json());
+};
+/**
+ * @description ID로 검색된 고양이 사진
+ * @param {String} id
+ * @returns {Object}
+ */
+API.getCat = id => {
+  return fetch(`${API_ENDPOINT}/cats/${id}`).then(res => res.json());
+};
+/**
+ * @description 랜덤한 50개의 고양이 사진 목록
+ * @returns {Array}
+ */
+API.random50 = _ => {
+  return fetch(`${API_ENDPOINT}/cats/random50`).then(res => res.json());
 };
 
 export { API };
