d868ac877fb377464ec4d52ca1f9543a35ccb229
신유나 <yunadev01@gmail.com>
2021-03-22 14:56:08 +0900

    Image slide

--- search-page/src/components/ItemRandom.js
+++ search-page/src/components/ItemRandom.js
@@ -3,25 +3,68 @@ import Component from '../core/Component.js';
 export class ItemRandom extends Component {
   template() {
     const { randomList } = this.$props;
-    console.log('🚀 ~ template ~ randomList', randomList);
     return `
       <div class="slider__btn">
-      <button class="prev">왼쪽</button>
-      <button class="next">오른쪽</button>
+      <button class="prev">⬅</button>
+      <button class="next">➡</button>
       </div>
       <div class="slider__box">
-        ${randomList
-          .map(
-            item => `
-          <article id="${item.id}" class="slider__item">
-            <img src="${item.url}" alt="item.name"/>
-          </article>
-        `
-          )
-          .join('')}
+        <ul class="slider__list">
+          ${
+            randomList.length === 0
+              ? `<p class="slider__list-txt">🐈  곧 50 냥이가 도착한다 냥 🐈</p>`
+              : randomList
+                  .map(
+                    item => `
+                <li id="${item.id}" class="slider__list-item">
+                  <img src="${item.url}" alt="item.name"/>
+                </li>
+                `
+                  )
+                  .join('')
+          }
+        </ul>
       </div>
     `;
   }
-}
 
-// 화면에 5개만 노출, 좌우 슬라이드 이동, 트렌지션
+  setEvent() {
+    const { randomList } = this.$props;
+    const prev = this.$target.querySelector('.slider__btn .prev');
+    const next = this.$target.querySelector('.slider__btn .next');
+    const slider = this.$target.querySelector('.slider__box .slider__list');
+    const firstChild = slider.firstElementChild.cloneNode(true);
+    const lastChild = slider.lastElementChild.cloneNode(true);
+    const length = randomList.length;
+    const size = 330;
+    let count = 1;
+
+    slider.appendChild(firstChild);
+    slider.insertBefore(lastChild, slider.firstElementChild);
+
+    prev.addEventListener('click', e => {
+      slider.style.transition = `transform ease-in-out 500ms`;
+      count--;
+      slider.style.transform = `translateX(-${count * size}px)`;
+      if (count <= 0) {
+        count = length - 4;
+        setTimeout(() => {
+          slider.style.transition = 'none';
+          slider.style.transform = `translateX(-${count * size}px)`;
+        }, 500);
+      }
+    });
+    next.addEventListener('click', e => {
+      slider.style.transition = `transform ease-in-out 500ms`;
+      count++;
+      slider.style.transform = `translateX(-${count * size}px)`;
+      if (count >= length - 4) {
+        count = 1;
+        setTimeout(() => {
+          slider.style.transition = 'none';
+          slider.style.transform = `translateX(-${size}px)`;
+        }, 500);
+      }
+    });
+  }
+}
